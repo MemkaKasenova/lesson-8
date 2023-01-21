a7c16ea261c615e8e81a6683cf56c39677743c62
Memka <mkasenova87@gmail.com>
2023-01-21 20:05:26 +0600

    дополнено

--- lesson 8/main.swift
+++ lesson 8/main.swift
@@ -7,5 +7,41 @@
 
 import Foundation
 
-print("Hello, World!")
+//Игра - Кто хочет стать миллионером?
+
+var questions: [String: [String:[String: Bool]]] =
+["В какие годы проходила вторая мировая?": ["a": ["1941-1945" : false], "b": ["1939-1945": true], "c": ["1937-1945": false], "d": ["1914-1917": false]],
+ "Год образования Гиктек?" : ["a": ["2019": false], "b": ["2017": false], "c": ["2018":true], "d": ["2020" :false]],
+ "Столица Португалии" : ["a": ["лиссабон":true], "b": ["париж":false], "c": ["мадрид":false], "d": ["порту":false]]
+]
+
+var prize: Int = 0
+
+var isFinish = true
+
+for (question,answers) in questions {
+    if isFinish == true {
+        break
+    }
+    print("\(question) варианты ответов: \(answers) ")
+    
+    print("Ваш ответ")
+    var answer = readLine()
+    
+    for (variant, variants) in answers {
+        
+        for (otvet,correct) in variants {
+            if answer == otvet && correct == true {
+                prize += 1000
+                print("Правильный ответ, у вас \(prize) сомов!")
+                break
+            } else {
+                isFinish = true
+                print("Неправильный ответ!")
+                break
+            }
+        }
+    }
+    print(prize)
+}
 
